62ad91c18c391ff8952512314e86002ac189c3e3
David Ensign <djensign@eagle.fgcu.edu>
2018-11-30 09:42:21 -0500

    11/30 - AM
    Edits in class to GUI, also added JAR file for FXTutorial 6

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("The Assignment Planner");
-        primaryStage.setScene(new Scene(root, 641, 290));
+        primaryStage.setScene(new Scene(root, 641, 273));
         primaryStage.show();
     }
 
